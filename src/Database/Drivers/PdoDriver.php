nette/database

added Nette\Database\QueryException

src/Database/Drivers/PdoDriver.php
@@ -58,9 +58,8 @@ abstract class PdoDriver implements Nette\Database\Driver
 			return new PdoResultDriver($statement, $this);
 
 		} catch (PDOException $e) {
-			$e = $this->convertException($e);
-			$e->queryString = $queryString;
-			$e->params = $params;
+			$e = $this->convertException($e, Nette\Database\QueryException::class);
+			$e->setQueryString($queryString, $params);
 			throw $e;
 		}
 	}

src/Database/QueryException.php
@@ -1,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (https://nette.org)
+ * Copyright (c) 2004 David Grudl (https://davidgrudl.com)
+ */
+
+declare(strict_types=1);
+
+namespace Nette\Database;
+
+
+class QueryException extends DriverException
+{
+	public string $queryString;
+	private array $params;
+
+
+	/** @internal */
+	public function setQueryString(string $queryString, array $params): void
+	{
+		$this->queryString = $queryString;
+		$this->params = $params;
+	}
+
+
+	public function getQueryString(): string
+	{
+		return $this->queryString;
+	}
+
+
+	public function getParameters(): array
+	{
+		return $this->params;
+	}
+}

jan-bures/nette-database

src/Database/Drivers/PdoDriver.php
@@ -62,9 +62,8 @@ abstract class PdoDriver implements Nette\Database\Driver
 			return new PdoResultDriver($statement, $this);
 
 		} catch (PDOException $e) {
-			$e = $this->convertException($e);
-			$e->queryString = $queryString;
-			$e->params = $params;
+			$e = $this->convertException($e, Nette\Database\QueryException::class);
+			$e->setQueryInfo($queryString, $params);
 			throw $e;
 		}
 	}

src/Database/QueryException.php
@@ -1,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (https://nette.org)
+ * Copyright (c) 2004 David Grudl (https://davidgrudl.com)
+ */
+
+declare(strict_types=1);
+
+namespace Nette\Database;
+
+
+class QueryException extends DriverException
+{
+	public string $queryString;
+	private array $params;
+
+
+	/** @internal */
+	public function setQueryInfo(string $queryString, array $params): void
+	{
+		$this->queryString = $queryString;
+		$this->params = $params;
+	}
+
+
+	public function getQueryString(): string
+	{
+		return $this->queryString;
+	}
+
+
+	public function getParameters(): array
+	{
+		return $this->params;
+	}
+}